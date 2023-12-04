8d3485e5981e3f3539db938bebe948dd5f888ebc
marcinswierkot <marc.swierkot@gmail.com>
2023-12-04 23:41:23 +0100

    [iot-2-exploratory-analysis-tags] Add query base class

--- src/main/java/org/iotdata/Runner.java
+++ src/main/java/org/iotdata/Runner.java
@@ -3,10 +3,15 @@ package org.iotdata;
 import static org.iotdata.reader.FileReader.readTTLsFromDirectoryToStream;
 
 import org.apache.jena.sparql.graph.GraphFactory;
+import org.iotdata.analysis.TagsAnalysisService;
 
 public class Runner {
-	public static void main(String[] args) {
-		var graph = GraphFactory.createDefaultGraph();
-		readTTLsFromDirectoryToStream("/cameras", graph);
-	}
+    public static void main(String[] args) {
+        var graph = GraphFactory.createDefaultGraph();
+        readTTLsFromDirectoryToStream("src/main/resources/tags/000/779", graph);
+
+        var tagAnalysisService = new TagsAnalysisService(graph);
+        tagAnalysisService.performExploratoryAnalysisIdentifier();
+        tagAnalysisService.performExploratoryAnalysisBattery();
+    }
 }
